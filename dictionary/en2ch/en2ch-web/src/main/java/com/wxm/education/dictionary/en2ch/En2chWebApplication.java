3b180e1146bda3d465b35b06df636eef9ba6dd5c
wuxm <15109870670@139.com>
2017-10-30 14:12:41 +0800

    优化代码：引入多环境配置

--- dictionary/en2ch/en2ch-web/src/main/java/com/wxm/education/dictionary/en2ch/En2chWebApplication.java
+++ dictionary/en2ch/en2ch-web/src/main/java/com/wxm/education/dictionary/en2ch/En2chWebApplication.java
@@ -1,5 +1,6 @@
 package com.wxm.education.dictionary.en2ch;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,13 +9,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <b>Title:</b> SpringBoot启动入口<br>
  * <b>Description:</b> <br>
  * <b>Date:</b> 2017年10月30日 上午11:42:05<br>
+ * 
  * @author wuxm
  * @version 1.0.0
  */
 @SpringBootApplication
 public class En2chWebApplication {
+    private final static String SPRING_PROFILES_ACTIVE = "spring.profiles.active=";
 
-	public static void main(String[] args) {
-		SpringApplication.run(En2chWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        if (ArrayUtils.isNotEmpty(args)) {
+            for (String arg : args) {
+                if (arg.startsWith(SPRING_PROFILES_ACTIVE)) {
+                    SpringApplication.run(En2chWebApplication.class, args).getEnvironment()
+                            .setActiveProfiles(arg.replace(SPRING_PROFILES_ACTIVE, ""));
+                }
+            }
+        } else {
+            SpringApplication.run(En2chWebApplication.class, args);
+        }
+    }
 }
--- dictionary/en2ch/en2ch-web/src/main/java/com/wxm/education/dictionary/en2ch/web/sm/SMUserController.java
+++ dictionary/en2ch/en2ch-web/src/main/java/com/wxm/education/dictionary/en2ch/web/sm/SMUserController.java
@@ -5,6 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * <b>Title:</b> 系统用户请求处理<br>
  * <b>Description:</b> <br>
  * <b>Date:</b> 2017年10月30日 上午11:42:36<br>
+ * 
  * @author wuxm
  * @version 1.0.0
  */
@@ -30,9 +32,10 @@ public class SMUserController {
         return "更新用户";
     }
 
+    @Path("{name}")
     @GET
-    public String list() {
-        return "用户列表";
+    public String list(@PathParam("name") String code) {
+        return "用户列表:" + code;
     }
 
     @DELETE
