e3981f606e79db153048c173bd3d3a62db0508e8
wuxiaomin <15109870670@139.com>
2017-11-15 23:39:31 +0800

    优化BaseInsertMapper

--- dictionary/en2ch/en2ch-dao/src/main/java/com/wxm/mybatis/mapper/provider/base/BaseInsertProvider.java
+++ dictionary/en2ch/en2ch-dao/src/main/java/com/wxm/mybatis/mapper/provider/base/BaseInsertProvider.java
@@ -112,6 +112,8 @@ public class BaseInsertProvider extends MapperTemplate {
             // 自增的情况下,如果默认有值,就会备份到property_cache中,所以这里需要先判断备份的值是否存在
             if (column.isIdentity()) {
                 sql.append(SqlHelper.getIfCacheNotNull(column, column.getColumnHolder(null, "_cache", ",")));
+            } else if (column.isUuid()) {
+                sql.append(SqlHelper.getIfCacheNotNull(column, column.getColumnHolder(null, "_cache", ",")));
             } else {
                 // 其他情况值仍然存在原property中
                 sql.append(SqlHelper.getIfNotNull(column, column.getColumnHolder(null, null, ","), isNotEmpty()));
@@ -123,7 +125,8 @@ public class BaseInsertProvider extends MapperTemplate {
             } else if (column.isIdentity()) {
                 sql.append(SqlHelper.getIfCacheIsNull(column, column.getColumnHolder() + ","));
             } else if (column.isUuid()) {
-                sql.append(SqlHelper.getIfIsNull(column, column.getColumnHolder(null, "_bind", ","), isNotEmpty()));
+                sql.append(SqlHelper.getIfCacheIsNull(column, column.getColumnHolder() + ","));
+                // sql.append(SqlHelper.getIfIsNull(column, column.getColumnHolder(null, "_bind", ","), isNotEmpty()));
             } else {
                 // 当null的时候，如果不指定jdbcType，oracle可能会报异常，指定VARCHAR不影响其他
                 sql.append(SqlHelper.getIfIsNull(column, column.getColumnHolder(null, null, ","), isNotEmpty()));
