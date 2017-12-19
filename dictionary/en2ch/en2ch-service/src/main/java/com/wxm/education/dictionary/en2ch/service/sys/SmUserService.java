0aa19b0a7db7bb06f2fcfbf50bc9e888787e0dc7
wuxiaomin <15109870670@139.com>
2017-12-19 23:15:43 +0800

    优化代码结构

--- dictionary/en2ch/en2ch-service/src/main/java/com/wxm/education/dictionary/en2ch/service/sys/SmUserService.java
+++
@@ -1,7 +0,0 @@
-package com.wxm.education.dictionary.en2ch.service.sys;
-
-import com.wxm.service.ICRUDService;
-
-public interface SmUserService<T, V> extends ICRUDService<T, V> {
-
-}
--- dictionary/en2ch/en2ch-service/src/main/java/com/wxm/education/dictionary/en2ch/service/sys/impl/SmUserServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.wxm.education.dictionary.en2ch.service.sys.impl;
-
-import com.wxm.service.abs.ACRUDService;
-
-public class SmUserServiceImpl<T, V, D, Q> extends ACRUDService<T, V, D, Q> {
-}
